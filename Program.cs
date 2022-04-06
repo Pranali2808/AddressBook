1d2e86c9320b736b786668c344868c40181ac969
pranali2808 <ranepranali1997@gmail.com>
2022-04-06 22:19:39 +0530

    create my contact details book

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome To Address Book Program on Master ");
+            CreateContact contactdetails = new CreateContact("Pranali", "Rane", "Bhusawal", "Jalgaon", "Maharashtra", 425305, 260101, "ranepranali1997h@gmail.com");
+            Console.WriteLine("Contact Detail is Created for : " + contactdetails.Firstname + " " + contactdetails.Lastname);
+            Console.WriteLine("Address Is :" + contactdetails.Address);
+            Console.WriteLine("City Is : " + contactdetails.City);
+            Console.WriteLine("State Is : " + contactdetails.State);
+            Console.WriteLine("Zip Is : " + contactdetails.Zip);
+            Console.WriteLine("Phone Number Is : " + contactdetails.PhoneNumber);
+            Console.WriteLine("Email Is : " + contactdetails.Email);
             Console.ReadLine();
         }
     }
